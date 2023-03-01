8464ab727c122f9b1707b664d9db7f39f357ee59
jongwony <lastone9182@gmail.com>
2023-03-02 00:41:18 +0900

    remove children: 5:27  Error: Do not pass children as props. Instead, nest children between the opening and closing tags.  react/no-children-prop 25

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -2,5 +2,9 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 export const convert = (markdown: string) => {
-    return <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+    return (
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {markdown}
+        </ReactMarkdown>
+    )
 }
