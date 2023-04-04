151b1759da2be22160e6b07d632c058a6fffea46
jongwony <lastone9182@gmail.com>
2023-04-04 09:36:12 +0900

    change title

--- src/components/gnb.tsx
+++ src/components/gnb.tsx
@@ -4,13 +4,10 @@ export default function TopNav() {
             <div className="max-w-8xl mx-auto">
                 <div className="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
                     <div className="relative flex items-center">
-                        <p>Jongwony Data Archive</p>
+                        <a href="//www.jongwony.com" target="_blank" className='hover:text-sky-500 dark:hover:text-sky-400'>Jongwony</a>
                         <div className="relative lg:flex items-center ml-auto">
                             <nav className="text-sm leading-6 font-semibold text-slate-700 dark:text-slate-200">
                                 <ul className="flex space-x-8">
-                                    <li>
-                                        <a href="//www.jongwony.com" target="_blank" className='hover:text-sky-500 dark:hover:text-sky-400'>About</a>
-                                    </li>
                                     <li>
                                         <a href="//home.jongwony.com" target="_blank" className='hover:text-sky-500 dark:hover:text-sky-400'>Home</a>
                                     </li>
